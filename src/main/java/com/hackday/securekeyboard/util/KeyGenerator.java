b16ec7fb41379a2de13479de1928b7d013d400b3
ihaeeun <ihaeeun16@gmail.com>
2019-05-17 01:10:38 +0900

    [#16] 암호화 util 추가

---
+++ src/main/java/com/hackday/securekeyboard/util/KeyGenerator.java
@@ -0,0 +1,37 @@
+package com.hackday.securekeyboard.util;
+
+import javax.crypto.Cipher;
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+
+public class KeyGenerator {
+    public void keyGen() {
+        try {
+            //공개키 및 개인키 생성
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+
+            KeyPair keyPair = keyPairGenerator.genKeyPair();
+            Key publicKey = keyPair.getPublic(); // 공개키
+            Key privateKey = keyPair.getPrivate(); // 개인키
+
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            RSAPublicKeySpec publicKeySpec = keyFactory.getKeySpec(publicKey, RSAPublicKeySpec.class);
+            RSAPrivateKeySpec privateKeySpec = keyFactory.getKeySpec(privateKey, RSAPrivateKeySpec.class);
+
+            System.out.println("public key modulus(" + publicKeySpec.getModulus() + ") exponent(" + publicKeySpec.getPublicExponent() + ")");
+            System.out.println("private key modulus(" + privateKeySpec.getModulus() + ") exponent(" + privateKeySpec.getPrivateExponent() + ")");
+
+            System.out.println("publicKey : " + publicKey.getEncoded().toString());
+            System.out.println("privateKey : " + privateKey.getEncoded().toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
